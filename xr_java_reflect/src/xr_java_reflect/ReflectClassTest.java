4609fcc4c53be4e22a323846d6b3daceaed82c4e
IamXiaRui <iamxiarui@foxmail.com>
2016-03-14 23:42:08 +0800

    反射类的使用

--- xr_java_reflect/src/xr_java_reflect/ReflectClassTest.java
+++ xr_java_reflect/src/xr_java_reflect/ReflectClassTest.java
@@ -1,7 +1,7 @@
 package xr_java_reflect;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -11,10 +11,36 @@ public class ReflectClassTest {
 	public void test_one() throws Exception {
 		// 反射无参类
 		Class class_one = Class.forName("xr_java_reflect.Person");
-		Constructor c_one = class_one.getConstructor(null);
+		Constructor c_one = class_one.getConstructor(null);// public
 		Person p_one = (Person) c_one.newInstance(null);
 		System.out.println(p_one.name);
-
 	}
 
+	@Test
+	public void test_two() throws Exception {
+		// 反射一个参数类
+		Class class_two = Class.forName("xr_java_reflect.Person");
+		Constructor c_two = class_two.getConstructor(String.class);// public
+		Person p_two = (Person) c_two.newInstance("name");
+		System.out.println(p_two.name);
+	}
+
+	@Test
+	public void test_three() throws Exception {
+		// 反射一个参数类
+		Class class_three = Class.forName("xr_java_reflect.Person");
+		Constructor c_three = class_three.getConstructor(String.class, int.class);// public
+		Person p_three = (Person) c_three.newInstance("name", 10);
+		System.out.println(p_three.name);
+	}
+
+	@Test
+	public void test_four() throws Exception {
+		// 反射一个参数类
+		Class class_four = Class.forName("xr_java_reflect.Person");
+		Constructor c_four = class_four.getDeclaredConstructor(List.class); // private
+		c_four.setAccessible(true);// 暴力反射
+		Person p_four = (Person) c_four.newInstance(new ArrayList());
+		System.out.println(p_four.name);
+	}
 }
