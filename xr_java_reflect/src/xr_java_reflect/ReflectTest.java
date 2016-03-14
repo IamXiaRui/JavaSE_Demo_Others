ddfc29fb8624c7c910c9869a07767e7ed7936e04
IamXiaRui <iamxiarui@foxmail.com>
2016-03-14 23:32:41 +0800

    反射加载类的三种方法

--- xr_java_reflect/src/xr_java_reflect/ReflectTest.java
+++ xr_java_reflect/src/xr_java_reflect/ReflectTest.java
@@ -14,7 +14,7 @@ public class ReflectTest {
 	public static void main(String[] args) throws ClassNotFoundException {
 
 		// 加载类的第一种方法
-		Class class_one = Class.forName("/xr_java_reflect/src/xr_java_reflect/Person");
+		Class class_one = Class.forName("xr_java_reflect.Person");
 
 		// 加载类的第二种方法
 		Class class_two = new Person(null, 0).getClass();
@@ -24,4 +24,3 @@ public class ReflectTest {
 	}
 
 }
-
