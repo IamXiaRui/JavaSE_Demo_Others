c0b2b9965a6cd2310ad2d38284e923b9f0b8abee
IamXiaRui <iamxiarui@foxmail.com>
2016-03-17 20:42:10 +0800

    内省操作JavaBean属性

--- xr_java_introspector/src/xr_java_introspector/Person.java
+++ xr_java_introspector/src/xr_java_introspector/Person.java
@@ -1,7 +1,15 @@
 package xr_java_introspector;
+
 /*
  * 典型的JavaBean
  * 
+ * 1、属性是私有的 
+ * 
+ *2、属性拥有get 和  set 的方法
+ *
+ *3、属性的个数  看get 和 set方法的个数    
+ *
+ *4、每个JavaBean都有一个class的属性
  * 
  * */
 import java.util.List;
